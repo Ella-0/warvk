8f9454807eee05d7c0d5c103fac546430a3cb615
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2020-10-11 20:38:33 +0000

    fmt

--- src/ash.rs
+++ src/ash.rs
@@ -1,18 +1,14 @@
 use std::{
+    borrow::Cow,
     cell::RefCell,
-	borrow::Cow,
     ffi::{CStr, CString},
     rc::Rc,
 };
 
 use ash::{
-	version::{DeviceV1_0, EntryV1_0, InstanceV1_0},
-	Entry,
-	extensions::{
-		khr, ext
-	},
-	Instance,
-	vk
+    extensions::{ext, khr},
+    version::{DeviceV1_0, EntryV1_0, InstanceV1_0},
+    vk, Entry, Instance,
 };
 
 use smithay::wayland::compositor::{CompositorToken, SurfaceAttributes};
@@ -20,9 +16,9 @@ use smithay::wayland::compositor::{CompositorToken, SurfaceAttributes};
 use crate::{ctx::RenderCtx, shell::Roles, window_map::WindowMap};
 
 pub struct AshCtx {
-	instance: Instance,
-	debug_utils_loader: ext::DebugUtils,
-	debug_call_back: vk::DebugUtilsMessengerEXT,
+    instance: Instance,
+    debug_utils_loader: ext::DebugUtils,
+    debug_call_back: vk::DebugUtilsMessengerEXT,
 }
 
 unsafe extern "system" fn vulkan_debug_callback(
@@ -65,39 +61,43 @@ impl AshCtx {
         let engine_name = CString::new("Smithay").unwrap();
 
         let layer_names = [CString::new("VK_LAYER_KHRONOS_validation").unwrap()];
-		let layer_names_raw = layer_names
+        let layer_names_raw = layer_names
             .iter()
             .map(|name| name.as_ptr())
             .collect::<Vec<_>>();
 
-		let surface_exts = [khr::Surface::name(), khr::Display::name(), ext::DebugUtils::name()];
-		let surface_exts_raw = surface_exts
-			.iter()
-			.map(|name| name.as_ptr())
-			.collect::<Vec<_>>(); 
+        let surface_exts = [
+            khr::Surface::name(),
+            khr::Display::name(),
+            ext::DebugUtils::name(),
+        ];
+        let surface_exts_raw = surface_exts
+            .iter()
+            .map(|name| name.as_ptr())
+            .collect::<Vec<_>>();
 
-		let instance = {
-			let app_info = vk::ApplicationInfo::builder()
-				.application_name(&app_name)
-				.engine_version(0)
-				.engine_name(&engine_name)
-				.api_version(vk::make_version(1, 1, 0));
+        let instance = {
+            let app_info = vk::ApplicationInfo::builder()
+                .application_name(&app_name)
+                .engine_version(0)
+                .engine_name(&engine_name)
+                .api_version(vk::make_version(1, 1, 0));
 
-			let create_info = vk::InstanceCreateInfo::builder()
-				.application_info(&app_info)
-				.enabled_layer_names(&layer_names_raw)
-				.enabled_extension_names(&surface_exts_raw);
+            let create_info = vk::InstanceCreateInfo::builder()
+                .application_info(&app_info)
+                .enabled_layer_names(&layer_names_raw)
+                .enabled_extension_names(&surface_exts_raw);
 
-			unsafe {
-				entry.create_instance(&create_info, None)
-					.expect("Instance creation error!")
-			}
-		};
-
+            unsafe {
+                entry
+                    .create_instance(&create_info, None)
+                    .expect("Instance creation error!")
+            }
+        };
 
         let debug_utils_loader = ext::DebugUtils::new(&entry, &instance);
 
-		let debug_call_back = {
+        let debug_call_back = {
             let debug_info = vk::DebugUtilsMessengerCreateInfoEXT::builder()
                 .message_severity(
                     vk::DebugUtilsMessageSeverityFlagsEXT::ERROR
@@ -107,30 +107,29 @@ impl AshCtx {
                 .message_type(vk::DebugUtilsMessageTypeFlagsEXT::all())
                 .pfn_user_callback(Some(vulkan_debug_callback));
 
-
             unsafe {
-				debug_utils_loader
-                	.create_debug_utils_messenger(&debug_info, None)
-                	.unwrap()
-			}
-		};
-
+                debug_utils_loader
+                    .create_debug_utils_messenger(&debug_info, None)
+                    .unwrap()
+            }
+        };
 
         AshCtx {
-			instance,
-			debug_utils_loader,
-			debug_call_back
-		}
+            instance,
+            debug_utils_loader,
+            debug_call_back,
+        }
     }
 }
 
 impl Drop for AshCtx {
-	fn drop(&mut self) {
-		unsafe {
-			self.debug_utils_loader.destroy_debug_utils_messenger(self.debug_call_back, None);
-			self.instance.destroy_instance(None);
-		}
-	}
+    fn drop(&mut self) {
+        unsafe {
+            self.debug_utils_loader
+                .destroy_debug_utils_messenger(self.debug_call_back, None);
+            self.instance.destroy_instance(None);
+        }
+    }
 }
 
 impl RenderCtx for AshCtx {
